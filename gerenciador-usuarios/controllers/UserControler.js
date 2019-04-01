4dd52c979ea2b9267bba56ac4a8de8c86d3df0a7
Tiago Valdrich <tiago.valdrich@hotmail.com>
2019-03-31 22:15:40 -0300

    Creating the user exclusion

--- gerenciador-usuarios/controllers/UserControler.js
+++ gerenciador-usuarios/controllers/UserControler.js
@@ -56,7 +56,7 @@ class UserController {
                         <td>${Utils.dateFormat(result._register)}</td>
                         <td>
                             <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
-                            <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                            <button type="button" class="btn btn-danger btn-delete btn-xs btn-flat">Excluir</button>
                         </td>
                     `;
 
@@ -219,7 +219,7 @@ class UserController {
         content +=      '<td>' + Utils.dateFormat(dataUser.register) + '</td>';
         content +=      '<td>';
         content +=          '<button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>';
-        content +=          '<button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>';
+        content +=          '<button type="button" class="btn btn-danger btn-delete btn-xs btn-flat">Excluir</button>';
         content +=      '</td>';
         content += '</tr>';
     
@@ -234,6 +234,18 @@ class UserController {
     }
 
     addEventsTR(tr) {
+
+        tr.querySelector(".btn-delete").addEventListener("click", event => {
+
+            if (confirm("Deseja realmente excluir?")) {
+
+                tr.remove();
+                
+                this.updateCount();
+
+            }
+
+        });
 
         tr.querySelector(".btn-edit").addEventListener("click", event => {
 
