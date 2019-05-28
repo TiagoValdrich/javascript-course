50cd4da660b4ab8b72803354338bbf90ebb3348d
Tiago <tiago.valdrich@hotmail.com>
2019-05-28 07:59:02 -0300

    Creating a server using express

--- restful/index.js
+++ restful/index.js
@@ -1,39 +1,33 @@
-const http = require('http');
+const express = require('express');
 
-let server = http.createServer((req, res) => {
+let app = express();
 
-    switch (req.url) {
+app.get('/', (req, res) => {
 
-        case '/':
-
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/html');
-            res.end('<h1>Olá Mundo!</h1>');
-
-        break;
-
-        case '/users':
-
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'application/json');
-            res.end(JSON.stringify({
-                users: [
-                    {
-                        id: 1,
-                        name: 'Tiago',
-                        email: 'tiago@batata.com'
-                    }
-                ]
-            }));
-
-        break;
-
-    }
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>Olá!</h1>');
 
 });
 
-server.listen(3000, '127.0.0.1', () => {
+app.get('/users', (req, res) => {
 
-    console.log('Running');
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({
+        users: [
+            {
+                id: 1,
+                name: 'Tiago',
+                email: 'tiago@batata.com'
+            }
+        ]
+    });
+
+});
+
+app.listen(3000, '127.0.0.1', () => {
+
+    console.log('Server Running!');
 
 });
