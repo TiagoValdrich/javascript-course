1e0740efa1c1166637107b091cc09c30da695b2d
Tiago Valdrich <tiago.valdrich@hotmail.com>
2019-07-06 22:03:34 -0300

    Using express-generator to generate the client server for users management project.

--- restful/routes/index.js
+++ restful/routes/index.js
@@ -2,6 +2,6 @@ module.exports = app => {
     app.get('/', (req, res) => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/html');
-        res.end('<h1>Olá!</h1>');
+        res.end('<h1>Restful API!</h1>');
     });
 };
